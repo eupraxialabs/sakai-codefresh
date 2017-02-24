5b8ed818fd7a1b4c6f58f291960fdfad24a32bf4
juanjmerono <jjmerono@um.es>
2017-02-24 21:49:13 +0100

    Fix more path stuff.

--- maven/mvnsecurity.sh
+++ maven/mvnsecurity.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mvn -emp $MASTER_PASSWORD | sed -e 's/[\/&]/\\&/g' > $HOME/mvnmp.txt
 MASTER_PASSWORD_ENC=$(cat $HOME/mvnmp.txt)
-sed -i -e 's/##MASTER_PASS##/'$MASTER_PASSWORD_ENC'/g' ./codefresh/maven/settings-security.xml
+sed -i -e 's/##MASTER_PASS##/'$MASTER_PASSWORD_ENC'/g' ./maven/settings-security.xml
 mkdir -p /root/.m2/
-cp ./codefresh/maven/settings-security.xml /root/.m2/settings-security.xml
+cp ./maven/settings-security.xml /root/.m2/settings-security.xml
 mvn -ep $ORACLE_PASS | sed -e 's/[\/&]/\\&/g' > $HOME/mvnsrv.txt
 ORACLE_PASS_ENC=$(cat $HOME/mvnsrv.txt)
-sed -i -e 's/##ORA_USER##/'$ORACLE_USER'/g' ./codefresh/maven/settings.xml
-sed -i -e 's/##ORA_PASS##/'$ORACLE_PASS_ENC'/g' ./codefresh/maven/settings.xml
+sed -i -e 's/##ORA_USER##/'$ORACLE_USER'/g' ./maven/settings.xml
+sed -i -e 's/##ORA_PASS##/'$ORACLE_PASS_ENC'/g' ./maven/settings.xml
