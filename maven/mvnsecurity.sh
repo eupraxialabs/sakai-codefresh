e312c360e519387c715a202db696e0fc491b5d9c
juanjmerono <jjmerono@um.es>
2017-03-02 15:32:51 +0100

    Add verbosity.

--- maven/mvnsecurity.sh
+++ maven/mvnsecurity.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ! -z $MASTER_PASSWORD ] 
 then
+	echo "Encrypting maven settings..."
 	mvn -emp $MASTER_PASSWORD | sed -e 's/[\/&]/\\&/g' > $HOME/mvnmp.txt
 	MASTER_PASSWORD_ENC=$(cat $HOME/mvnmp.txt)
 	sed -i -e 's/##MASTER_PASS##/'$MASTER_PASSWORD_ENC'/g' ./maven/settings-security.xml
@@ -10,8 +11,13 @@ then
 	ORACLE_PASS_ENC=$(cat $HOME/mvnsrv.txt)
 	sed -i -e 's/##ORA_USER##/'$ORACLE_USER'/g' ./maven/settings.xml
 	sed -i -e 's/##ORA_PASS##/'$ORACLE_PASS_ENC'/g' ./maven/settings.xml
+else
+	echo "Missing master password..."
 fi
 if [ "$DEBUG_ENV" == "true" ]
 then
+	echo "Debugging maven encryption..."
 	grep -e 'user\|pass\|master' ./maven/*.xml
+else
+	echo "Not debugging..."
 fi
